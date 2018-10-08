916e9ec091308db5cd4b739ca0ed345663efc9d4
John Azzaro <jazzaro@temple.edu>
2018-10-07 21:37:57 -0700

    html addition

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ function generateStartPage() {
     return `
     <div class="css-container">
         <h2>Welcome to the Original Shopping List App!</h2>
-        <p>Ever gone to grocery store and forgot that critical item for your spaghetti meatsauce?</p>
-        <p>With the Original Shopping App, you can do many amazing things, such as:</p>
+        <p>Are you finding it hard to keep track of all the grocery items you need to buy? </p>
+        <p>With the Original Shopping App, you'll never forget a thing!</p>
+        <p>Some of the amazing features of this app include:</p>
         <ul>        
-            <li>Keep track of your necessary grocery items.</li>
+            <li>Store and keep track of unlimited items.</li>
             <li>Add items to your grocery list</li>
         </ul>
         <p>So what are you waiting for?  Start the shopping app now!</p>
